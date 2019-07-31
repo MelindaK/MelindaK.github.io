4b4bb2de4ce234a2eb293b8a4625752b6a7ce903
Melinda Kilner <mhkilner@gmail.com>
2019-07-31 15:58:47 -0700

    fix typo

--- src/pages/index.js
+++ src/pages/index.js
@@ -13,7 +13,7 @@ const IndexPage = () => (
     <div className="layout-container ">
     <section className="section-intro">
       <h1 className="title">Hello!</h1>
-	    <p>I’m Melinda, a product designer living in San Francisco. I’ve worked extensively in user experience design and front-end web development and I find my groove anywhere I can dig into hard problems, make people’s lives a tiny bit easer, and learn something new every day. Feel free to check out some of my projects below.</p>
+	    <p>I’m Melinda, a product designer living in San Francisco. I’ve worked extensively in user experience design and front-end web development and I find my groove anywhere I can dig into hard problems, help make people’s lives a tiny bit easier, and continue learning. Feel free to check out some of my projects below.</p>
     </section>
     </div>
     <section id="projects" className="section-bleed-bg">
